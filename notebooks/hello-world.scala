8bb2d7bbe7dd40aa0149cea06e1ab2afe4dff960
luis-israel-velez <luis.israel.velez@gmail.com>
2019-09-22 13:30:07 -0400

    f

--- notebooks/hello-world.scala
+++ notebooks/hello-world.scala
@@ -1,6 +1,2 @@
 // Databricks notebook source
-<<<<<<< HEAD
-print("Hello World! Well this is awesome! Right!")
-=======
 print("Hello World! Well this is awesome!")
->>>>>>> facce426e59a13d6c62009eac8250a1f011a619e
