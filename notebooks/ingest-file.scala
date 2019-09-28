d722e11eac7545011ee1a6d8263f2a81d8cf1f94
luis-israel-velez <40527357+luis-israel-velez@users.noreply.github.com>
2019-09-28 17:43:14 -0400

    V 1.0

--- notebooks/ingest-file.scala
+++ notebooks/ingest-file.scala
@@ -1,25 +1,54 @@
 // Databricks notebook source
+//The following notebook will read the storage account that contains the real live transactions 
+//TODO: Parameters? Dynamic execution (contract = schema/etc)
+
+// COMMAND ----------
+
 // MAGIC %run ./setup
 
 // COMMAND ----------
 
-println(dbutils.fs.ls(path))
+val df = spark
+  .read.format("avro")
+  .option("infereSchema", true)
+  .load(path+"landing/lv-iot-streaming/01/2019/09/28/*") //TODO: Parametes?
 
 // COMMAND ----------
 
-val df = spark.read.option("inferSchema", true).json(path+"lv-iot-streaming/01/2019/09/23/21/47") //.option("header", true)
+
+val impressionSchema = new StructType()
+    .add("EntityType", StringType, true)
+    .add("impressionDate", StringType, true)
+    .add("advertID", StringType, true)
+    .add("impressionURL", StringType, true)
+    .add("sessionID", StringType, true)
+    .add("firstName", StringType, true)
+    .add("lastName", StringType, true)
+
+
+val impressionsDF = df
+  .select($"body".cast("string").alias("json"))
+  .withColumn("data", from_json(col("json"), impressionSchema))
+  .filter($"data.EntityType" === "Impressions")
+  .select("data.*")
+
+//display(impressionsDF)
 
 // COMMAND ----------
 
-display(df)
 
-// COMMAND ----------
+val advertsSchema = new StructType()
+    .add("EntityType", StringType, true)
+    .add("createdDate", StringType, true)
+    .add("advertID", StringType, true)
+    .add("advertName", StringType, true)
+    .add("brand", StringType, true)
 
-//Read if needed
-df
- .coalesce(1)
- .write
- .mode("overwrite")
- .option("header", "true")
- .format("com.databricks.spark.csv")
- .save(path)
+
+val advertsDF = df
+  .select($"body".cast("string").alias("json"))
+  .withColumn("data", from_json(col("json"), advertsSchema))
+  .filter($"data.EntityType" === "Adverts")
+  .select("data.*")
+
+//display(advertsDF)
